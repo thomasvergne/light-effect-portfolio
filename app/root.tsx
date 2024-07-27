775c197a23d4666cd1e34d86faedc702074f0258
Thomas Vergne <contact@thomas-vergne.fr>
2024-07-27 18:39:52 +0200

    feat(remix): added base typescript files

---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import "./tailwind.css";
+import { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => {
+  // Adding PNG favicon
+  return [
+    { rel: "icon", type: "image/png", href: "/picture.png" },
+  ];
+}
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="dark:bg-black bg-white overflow-x-hidden">
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
